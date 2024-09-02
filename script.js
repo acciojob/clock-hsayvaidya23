e6e3e2075ee498d31cca1091c832c33ffb3138f8
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 17:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timeDis = document.getElementById('timer');
+let currDate = new Date();
+setInterval(
+	() => {
+		currDate = new Date();
+		timeDis.innerHTML = currDate.toLocaleString();
+	}
+, 1000)
